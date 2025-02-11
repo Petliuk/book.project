10caa38e8d6169d8dd2dcf13df8b5c75835305ab
Maks <petliukofficial@gmail.com>
2025-02-11 12:38:24 +0200

    implementation of the task 'Add JWT support'

---
+++ src/main/java/com/example/dto/UserLoginRequestDto.java
@@ -0,0 +1,17 @@
+package com.example.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+@Data
+public class UserLoginRequestDto {
+    @NotBlank
+    @Length(min = 8, max = 20)
+    @Email
+    private String email;
+    @NotBlank
+    @Length(min = 8, max = 20)
+    private String password;
+}
